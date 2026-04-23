c376e7d180d3c83f538357d1c0da08c54ee96ee3
Ailton Junior <ailtonjunior726@gmail.com>
2026-04-23 11:20:59 -0300

    Adição parquets monetarizados em processed

--- notebooks/teste.py
+++ notebooks/teste.py
@@ -24,3 +24,25 @@ epi_monetary_ufs_sh6.sample(10)
 
 #%%
 epi_monetary_ufs.sample(10)
+
+#%% 
+sg_uf = epi_monetary_ufs_country.select(pl.col('sg_uf').unique())
+
+print(sg_uf)
+
+#%%
+
+sg_uf_sh6 = epi_monetary_ufs_sh6.select(pl.col('sg_uf').unique())
+
+print(sg_uf_sh6)
+
+#%% 
+sg_uf_sh6 = epi_monetary_ufs_sh6.select(pl.col('sg_uf').unique())
+
+print(sg_uf_sh6)
+
+#%% 
+
+sg_uf_total = epi_monetary_ufs.select(pl.col('sg_uf').unique())
+
+print(sg_uf_sh6)
